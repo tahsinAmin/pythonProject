69bd3c78a34f4703c9a038bafc5510f236f130dc
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-01-10 12:50:14 +0600

    How to get all images with others as well

---
+++ README.md
@@ -0,0 +1,22 @@
+Disclaimer: The OS & browser used for this project is Ubuntu Firefox, so the options that will be mentioned in this project or readme file are right for Ubuntu Firefox.
+
+#Task
+- [x] how to limit number of scraps
+- [x] how toi take image src
+- [x] how to extract and then join themk with a string.
+- [x] how to take image link when the class name varies
+- [x] How to get all images
+- [ ] How to skip the element which has no image
+
+# What I learned
+
+- Installing Python from (Installing Python 3.7 on Ubuntu with Apt)[https://linuxize.com/post/how-to-install-python-3-7-on-ubuntu-18-04/]
+
+
+
+(Pycharm)[https://www.youtube.com/watch?v=15daSz2QExo]
+
+# Problems and Solutions
+
+- Pycharm couldn’t find the interpreter. (Solution)[https://askubuntu.com/questions/1376798/cannot-use-python-3-10-as-interpreter-on-pycharm]
+- Fonts to be bigger in Pycharm. Sol: https://www.youtube.com/watch?v=xZjoLK0yuMk
--- content_spider.py
+++ content_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 
 class ContentSpider(scrapy.Spider):
@@ -6,26 +7,27 @@ class ContentSpider(scrapy.Spider):
     start_urls = [
         'https://www.kayak.co.in/Hyderabad-Hotels.7297.hotel.ksp',
     ]
+
     # ihbo
 
     def parse(self, response):
-        counter=1
+
+        counter = 1
         for content in response.css('div.soom'):
-            # amen = content.css('span.yRv1-text').extract()
             if counter < 11:
+                title = content.xpath('div/div/div/a/span/text()').get()
                 list1 = content.css('span.yRv1-text::text').extract()
                 list_to_string = ",".join(list1)
                 demo = content.xpath('a/img/@src').get()
-                if demo is None:
-                    demo = content.xpath('a/span/text()').get()
-                    print(demo)
-                else:
-                    print(demo)
-                # ,
-                # 'image_link': content.xpath('//a[@class="soom-photo-wrapper"]').css('img::attr(src)').get(),
+
+                if demo == None:
+                    try1 = response.xpath(f"(//script[contains(text(),'{title}')])/text()").getall()
+                    data = json.loads(try1[0])
+                    demo = data.get('image')
+
                 yield {
-                    'image_link': content.xpath('a/img/@src').get(),
-                    'title': content.xpath('div/div/div/a/span/text()').get(),
+                    'image_link': demo,
+                    'title': title,
                     'ratings': content.xpath('div/div/div/div/span/text()').get(),
                     'location': content.css('span.soom-neighborhood::text').get(),
                     'amenities': list_to_string,
@@ -34,8 +36,6 @@ class ContentSpider(scrapy.Spider):
                 counter += 1
             else:
                 break
-
-
 
         next_page = response.css('li.next a::attr("href")').get()
         if next_page is not None:
